27700aa0105603286beb9c7ba6629147638498d3
Mika Reivari <mika.reivari@mbnet.fi>
2024-03-16 22:21:36 +0200

    Initial commit

---
+++ src/main/java/fi/mikareivari/demo/springthymeleafexamples/SpringThymeleafExamplesApplication.java
@@ -0,0 +1,13 @@
+package fi.mikareivari.demo.springthymeleafexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/mikareivari/demo/springthymeleafexamples/SpringThymeleafExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.mikareivari.demo.springthymeleafexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringThymeleafExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
